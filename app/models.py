03a70667512817cd307e7a5562bd9c9fb69238fa
laxier <hash0xcb8c@yandex.ru>
2024-04-13 20:36:43 +0300

    made a form to submit the new task

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 
 class ToDo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Strong(200), nullable = False)
+    content = db.Column(db.String(200), nullable = False)
     date_created = db.Column(db.DateTime, default = datetime.utcnow())
 
     def __repr__(self):
--- app/views.py
+++ app/views.py
@@ -1,8 +1,12 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
+from app.models import ToDo
 
-@app.route('/')
-@app.route('/index')
+
+@app.route('/', methods=['POST', 'GET'])
 def index():  # put application's code here
-    username = "Kirill"
-    return render_template('index.html', username = username)
+    if request.method == "POST":
+        task = request.form['content']
+        new_task = ToDo(content = task)
+    else:
+        return render_template('index.html')
